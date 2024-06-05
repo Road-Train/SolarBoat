579a21be16b0103d4b7c4f7ddffc21c1165fe8e8
Road-Train <trol13xd@gmail.com>
2024-06-05 09:48:34 +0200

    Fixed manage.py

--- controller_dashboard/manage.py
+++ controller_dashboard/manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', './controller_dashboard/settings.py')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'controller_dashboard.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
