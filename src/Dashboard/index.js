133bc52d14012f7a974e63a17eb6d9409df72ce4
ViragSzabo <virag.szabo@student.nhlstenden.com>
2024-06-24 08:02:39 +0200

    React project - success (browser error)

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,10 +1,14 @@
 import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 function NavigationBar() {
   // TODO: Actually implement a navigation bar
   return <h1>Hello from React!</h1>;
 }
 
+ReactDOM.render(<App />, document.getElementById('root'));
 const domNode = document.getElementById('navigation');
 const root = createRoot(domNode);
 root.render(<NavigationBar />);
