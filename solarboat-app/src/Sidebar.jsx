a354903285bda69679c55c5dfbd1263753c58575
ViragSzabo <virag.szabo@student.nhlstenden.com>
2024-06-25 21:47:08 +0200

    Show dashboard

--- solarboat-app/src/Sidebar.jsx
+++ solarboat-app/src/Sidebar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
+
 const Sidebar = () => {
   return (
     <aside className="bg-gray-800 text-white w-64 min-h-screen p-4">
       <nav className="space-y-4">
-        <Link to="#" className="block py-2 px-4 rounded hover:bg-gray-700">3D Simulation</Link>
-        <Link to="#" className="block py-2 px-4 rounded hover:bg-gray-700">Object Recognition</Link>
-        <Link to="#" className="block py-2 px-4 rounded hover:bg-gray-700">NUC</Link>
+        <link rel="#" className="block py-2 px-4 rounded hover:bg-gray-700" name="ThreeD" />
+        <link rel="#" className="block py-2 px-4 rounded hover:bg-gray-700" name="OR" />
+        <link rel="#" className="block py-2 px-4 rounded hover:bg-gray-700" name="NUC" />
         </nav>
     </aside>
   );
