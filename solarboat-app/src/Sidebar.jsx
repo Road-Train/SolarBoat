145962b153197f2b2ea6b80cc2e6a31232403d5b
ViragSzabo <virag.szabo@student.nhlstenden.com>
2024-07-07 20:47:00 +0200

    Add logo

--- solarboat-app/src/Sidebar.jsx
+++ solarboat-app/src/Sidebar.jsx
@@ -5,6 +5,7 @@ const Sidebar = () => {
   return (
     <aside className="bg-gray-800 text-white w-64 min-h-screen p-4">
       <nav className="space-y-4">
+        <img src='/image.png' alt="Project Logo" className="logo"/>
         <Link to="/" className="block py-2 px-4 rounded hover:bg-gray-700">Home</Link>
         <Link to="/profile" className="block py-2 px-4 rounded hover:bg-gray-700">Profile</Link>
         <Link to="/simulation" className="block py-2 px-4 rounded hover:bg-gray-700">3D Simulation</Link>
